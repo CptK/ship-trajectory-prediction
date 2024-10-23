0ec609e0dfcae52f182cea0f22d03c3e8c188097
Marcus Kornmann <marcus.kornmann@icloud.com>
2024-10-23 08:55:15 +0300

    refactor haversine to own function

--- prediction/preprocessing/__init__.py
+++ prediction/preprocessing/__init__.py
@@ -1,3 +1,3 @@
-from .utils import calc_sog, timedelta_to_seconds
+from .utils import calc_sog, timedelta_to_seconds, haversine
 from .trajectory_collection import build_trajectories, load_and_build
 from .outlier_detection import remove_outliers, remove_outliers_parallel
--- prediction/preprocessing/utils.py
+++ prediction/preprocessing/utils.py
@@ -13,6 +13,25 @@ def timedelta_to_seconds(delta: timedelta):
     return total_seconds + (total_microseconds / 1000000)
 
 
+def haversine(lat1: float, lng1: float, lat2: float, lng2: float) -> float:
+    """This function calculates the haversine distance between two points given their coordinates.
+
+    Args:
+        lat1: Latitude of the first point
+        lng1: Longitude of the first point
+        lat2: Latitude of the second point
+        lng2: Longitude of the second point
+
+    Returns:
+        The haversine distance between the two points in kilometers.
+    """
+    delta_lat = lat2 - lat1
+    delta_lng = lng2 - lng1
+    a = (np.sin(delta_lat / 2) ** 2) + np.cos(lat1) * np.cos(lat2) * (np.sin(delta_lng / 2) ** 2)
+    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
+    return 6371 * c # haversine distance
+
+
 def calc_sog(lat1: float, lng1: float, lat2: float, lng2: float, time: float) -> float:
     """This function calculates the speed between two points given their coordinates and time difference.
 
@@ -26,9 +45,5 @@ def calc_sog(lat1: float, lng1: float, lat2: float, lng2: float, time: float) ->
         lng2: Longitude of the second point
         time: Time difference between the two points in seconds
     """
-    delta_lat = lat2 - lat1
-    delta_lng = lng2 - lng1
-    a = (np.sin(delta_lat / 2) ** 2) + np.cos(lat1) * np.cos(lat2) * (np.sin(delta_lng / 2) ** 2)
-    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
-    distance = 6371 * c # haversine distance
+    distance = haversine(lat1, lng1, lat2, lng2)
     return distance / (time + 1e-10) # add small number to avoid division by zero
