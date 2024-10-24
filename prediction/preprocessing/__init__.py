417705b4cafef29c5dcd69113bb7775d8ea8b096
Marcus Kornmann <marcus.kornmann@icloud.com>
2024-10-24 13:29:15 +0300

    add batched haversine version

--- prediction/preprocessing/__init__.py
+++ prediction/preprocessing/__init__.py
@@ -1,3 +1,3 @@
-from .utils import calc_sog, timedelta_to_seconds, haversine
+from .utils import calc_sog, timedelta_to_seconds, haversine, haversine_tensor
 from .trajectory_collection import build_trajectories, load_and_build
 from .outlier_detection import remove_outliers, remove_outliers_parallel
