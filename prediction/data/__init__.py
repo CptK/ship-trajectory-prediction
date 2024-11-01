c453a947b6928c97277a87603c8576919420b91c
Marcus Kornmann <marcus.kornmann@icloud.com>
2024-11-01 17:12:46 +0200

    formatting

--- prediction/data/__init__.py
+++ prediction/data/__init__.py
@@ -1,8 +1,8 @@
+from .code_mappings import status_codes, vessel_groups
 from .dataset import AISDataSet, MaskedAISDataset
 from .filter_data import filter_data
 from .loader import download_all_data, download_data, load_data
 from .tsspl_dataset import TSSPLDataset
-from .code_mappings import vessel_groups, status_codes
 
 __all__ = [
     "AISDataSet",
