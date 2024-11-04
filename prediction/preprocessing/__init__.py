d4b8befe55de79e13e6a8a6095b665e57c24acb9
Marcus Kornmann <marcus.kornmann@icloud.com>
2024-11-04 18:20:07 +0200

    functionality for resampling trajectories at fixed time intervals

--- prediction/preprocessing/__init__.py
+++ prediction/preprocessing/__init__.py
@@ -1,4 +1,5 @@
 from .outlier_detection import remove_outliers, remove_outliers_parallel
+from .resample_trajectories import resample_trajectories
 from .trajectory_collection import build_trajectories, load_and_build
 from .utils import calc_sog, haversine, haversine_tensor
 
@@ -10,4 +11,5 @@ __all__ = [
     "calc_sog",
     "haversine",
     "haversine_tensor",
+    "resample_trajectories",
 ]
