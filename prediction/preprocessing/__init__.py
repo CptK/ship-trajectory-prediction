9e740a963c77a711da732e408347c92699cf9880
Marcus Kornmann <marcus.kornmann@icloud.com>
2024-11-07 22:36:45 +0200

    bbox size linestring

--- prediction/preprocessing/__init__.py
+++ prediction/preprocessing/__init__.py
@@ -1,7 +1,7 @@
 from .outlier_detection import remove_outliers, remove_outliers_parallel
 from .trajectory_collection import build_trajectories, load_and_build
 from .trajectory_resampling import compare_trajectory_pairs, resample_trajectories
-from .utils import calc_sog, haversine, haversine_tensor, pairwise_point_distances, max_bbox_diagonal
+from .utils import calc_sog, haversine, haversine_tensor, max_bbox_diagonal, pairwise_point_distances
 
 __all__ = [
     "remove_outliers",
